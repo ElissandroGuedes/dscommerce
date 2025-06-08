3f3cce159a47611cd2f9f079b12136480ae4584e
Elissandro Guedes <edossantosguedes@gmail.com>
2025-06-08 11:47:46 -0300

    Orderitem and OrderItemPK

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -14,7 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String phone;
     private LocalDate birthDate;
     private String password;

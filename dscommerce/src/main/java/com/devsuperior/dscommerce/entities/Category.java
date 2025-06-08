027f64638a5f902bf4a8251c911bc29df7c683c2
Elissandro Guedes <edossantosguedes@gmail.com>
2025-06-08 16:03:17 -0300

    Equals e hascode

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -45,4 +45,17 @@ public class Category {
     public Set<Product> getProducts() {
         return products;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
 }
